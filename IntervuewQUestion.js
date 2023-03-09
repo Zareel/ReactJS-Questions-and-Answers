0719f5984f5996cc43ae2d3d3c2f3494fe996fdb
Zareel Kalam <zareelbwd@gmail.com>
2023-03-09 11:51:45 +0530

    first commit

---
+++ IntervuewQUestion.js
@@ -0,0 +1,64 @@
+//
+/*
+=> JSX
+=> What React ? fearures of React ?
+=> Babel, webpack ?
+=> DOM and Virtual Dom
+=> Components and props and Difference between both of them
+=> How virtual DOm Work
+=> What is State?
+=> Higher order Compenets
+=> React Fiber
+=> React Reconcilattion (IMP)
+=> react-dom package
+=> Statefull and State Less Component
+=> error Boundires
+=> props Drilling
+=> What is Hooks and why there re created  (useState, useEffect,useContext, useref, useMemo )
+=> Context API
+=> what is Conditinal Rendering
+=> Decorator in react
+=> Memoize Coponents
+=> React Mixins
+=> debouncing
+=> React Router // In depth
+=> Throttling
+=> Componet LifeCycle
+=> React tetsing Libraray
+=> React portals (for modals)
+=> what is Jest
+=> Shallow Render
+=> render props
+=> React Query
+=> How to render the react componnet when browser shrink
+=> State management (Redux toolkit)
+=> Ajax, jQuery
+=> Fragments
+=> What us CRA and it benifits
+=> How to Write core Css in React
+=> React with CDN
+=> What is Strict Mode in reactjS
+=> linters
+=> Devtools
+=> the Folder Structuer of React
+=> Fetch and Axios
+=> List map and Keys
+=> How to create Custom Hooks
+=> fetching Data using Hooks
+=> Events in React
+=> Form handling
+=> Redux vs Context API
+=> Code Splitting
+=> Lazy Loading
+=> MVC Architecture
+=> Flux in React
+*/
+
+let arr = [];
+let i = 0;
+
+while (i <= 5) {
+  arr.push(i);
+  i++;
+}
+console.log(arr.sort().reverse());
